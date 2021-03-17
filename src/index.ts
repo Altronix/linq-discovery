479196877be9dcac912e9ef42cdffd84b08dbbb9
Ricardo Levano <rlevano77@gmail.com>
2021-03-17 09:01:47 -0400

    Deprecating mdns switching to UDP Bcast

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,31 @@
-const bonjour = require('bonjour')()
 import { EventEmitter } from 'events'
-
-interface Txt {
-  manufacturer: string
-}
-
-interface Service {
-  addresses: string[]
-  name: string
-  port: number
-  txt: Txt
-}
-
+import dgram from 'dgram'
+const server = dgram.createSocket('udp4')
 class Device {
   product: string
   id: string
   ip: string
-  port: number
+  http: number
+  https: number
 
-  constructor(product: string, id: string, ip: string, port: number) {
+  constructor(
+    product: string,
+    id: string,
+    ip: string,
+    http: number,
+    https: number
+  ) {
     this.product = product
     this.id = id
     this.ip = ip
-    this.port = port
+    this.http = http
+    this.https = https
   }
 }
 
 export declare interface Discovery {
   on(event: 'new', listener: (msg: object) => void): this
 }
-
 export class Discovery extends EventEmitter {
   devices: Map<string, object>
   constructor() {
@@ -37,18 +33,18 @@ export class Discovery extends EventEmitter {
     this.devices = new Map()
   }
 
-  discovery = () => {
-    bonjour.find({ type: 'linq' }, (service: object) => {
-      let svc: Service = <Service>service
+  async start(port: number) {
+    await server.bind(port)
+    server.on('message', (msg: string) => {
+      let obj = JSON.parse(msg.toString().split('\n')[0]);
       let device: Device = new Device(
-        svc.name.split('-')[0], // product
-        svc.name.split('-')[1], // id
-        svc.addresses[0], // ipv4
-        svc.port // port number
+        obj.product,
+        obj.id,
+        obj.ip,
+        obj.http,
+        obj.https
       )
-
       if (!this.devices.has(device.id)) {
-        // if device is not in devices => add device to Map
         this.devices.set(device.id, device)
         this.emit('new', device)
       }
@@ -66,9 +62,5 @@ export class Discovery extends EventEmitter {
   clear() {
     this.devices.clear()
   }
-
-  async start(poll_period_ms: number) {
-    this.discovery()
-    setInterval(this.discovery, poll_period_ms) // Poll every 'poll_period_ms' milliseconds
-  }
 }
+
