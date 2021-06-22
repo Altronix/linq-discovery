5c2f3ba5da3bfa6592bd5ac4bace7be10186bb09
Ricardo Levano <rlevano77@gmail.com>
2021-06-22 17:11:20 -0400

    v2.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { EventEmitter } from 'events'
 import dgram from 'dgram'
 
-const UDP_BCAST_PORT = 7123
+const UDP_BCAST_PORT = 7123 /* Default PORT */
 const server = dgram.createSocket('udp4')
 
+interface Device {
+  version: number
+  manufacturer: string
+  family: string
+  product: string
+  id: string
+  ip: string
+  http?: number
+  https?: number
+  mqtt?: number
+  mqtts?: number
+}
+
 export declare interface Discovery {
-  on(event: 'new', listener: (msg: object) => void): this
-  on(event: 'message', listener: (msg: object) => void): this
+  on(event: 'new', listener: (msg: Device) => void): this
+  on(event: 'message', listener: (msg: Device) => void): this
 }
 
 export class Discovery extends EventEmitter {
-  devices: Map<string, object>
+  devices: Map<string, Device>
   constructor(port: number = UDP_BCAST_PORT) {
     super()
     this.devices = new Map()
@@ -19,18 +32,20 @@ export class Discovery extends EventEmitter {
 
   async start(port: number) {
     await server.bind(port)
-    server.on('message', (msg: string) => {
-      let obj = JSON.parse(msg.toString().split('\n')[0])
-      if (!this.devices.has(obj.id)) {
-        this.devices.set(obj.id, obj)
-        this.emit('new', obj)
+    server.on('message', (msg: object) => {
+      let obj: Device = JSON.parse(msg.toString())
+      if (obj.manufacturer.toUpperCase() === 'ALTRONIX') {
+        if (!this.devices.has(obj.id)) {
+          this.devices.set(obj.id, obj)
+          this.emit('new', obj)
+        }
+        this.emit('message', obj)
       }
-      this.emit('message', obj)
     })
   }
 
   get_devices() {
-    let arr: object[] = []
+    let arr: Device[] = []
     for (let value of this.devices.values()) {
       arr.push(value)
     }
